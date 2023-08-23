127c58aa452954b2e31ece92021b2fcfcc66bb90
supriyo97 <supriyam789@gmail.com>
2023-08-23 15:07:03 +0530

    Updated Utility

--- Utility.java
+++ Utility.java
@@ -1,30 +1,50 @@
 import java.util.Scanner;
 
 public class Utility {
-    public String getInLoc(){
-        System.out.println("First of all, provide me the input file location");
-        Scanner sc = new Scanner(System.in);
-        String inLoc = sc.nextLine();
+
+    public String getInLoc() {
+        String inLoc = "";
+        while (inLoc.trim().isEmpty() || !inLoc.toLowerCase().endsWith(".csv")) {
+            System.out.println("First of all, provide me the input file location (with .csv extension)");
+            Scanner sc = new Scanner(System.in);
+            inLoc = sc.nextLine().trim();
+            if (inLoc.isEmpty()) {
+                System.out.println("Please enter a valid input file location.");
+            } else if (!inLoc.toLowerCase().endsWith(".csv")) {
+                System.out.println("Please provide a file with .csv extension.");
+            }
+        }
         return inLoc;
     }
 
-    public String getOutLoc(){
-        System.out.println("If you want to save the data to other file then provide me the output file location");
-        Scanner sc = new Scanner(System.in);
-        String outLoc = sc.nextLine();
+    public String getOutLoc() {
+        String outLoc = "";
+        while (outLoc.trim().isEmpty() || !outLoc.toLowerCase().endsWith(".txt")) {
+            System.out.println("If you want to save the data to another file, provide me the output file location (with .txt extension)");
+            Scanner sc = new Scanner(System.in);
+            outLoc = sc.nextLine().trim();
+            if (outLoc.isEmpty()) {
+                System.out.println("Please enter a valid output file location.");
+            } else if (!outLoc.toLowerCase().endsWith(".txt")) {
+                System.out.println("Please provide a file with .txt extension.");
+            }
+        }
         return outLoc;
     }
 
-    public String getOpType(){
+    public int getOpType(){
+        System.out.println("\n-------------------------------------------------------");
         System.out.println("You can perform following operations to the CSV file.");
         System.out.println("1. Add data to it.");
         System.out.println("2. Edit the data.");
         System.out.println("3. Delete the data.");
-        System.out.println("4. Sort the data based on country name & save the data to other file.");
-        System.out.println("5. Convert the temperature to Fahrenheit & save the data to other file.");
-        System.out.print("Enter your choice : ");
+        System.out.println("4. Sort the data based on country name & save the data to output file.");
+        System.out.println("5. Convert the temperature to Fahrenheit & save the data to output file.");
+        System.out.println("6. Exit");
+        System.out.println("-------------------------------------------------------");
+        System.out.print("\nEnter your choice : ");
         Scanner sc = new Scanner(System.in);
-        String choice = sc.nextLine();
-        return choice;
+        int ch = sc.nextInt();
+        return ch;
     }
 }
